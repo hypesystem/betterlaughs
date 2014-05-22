52a553d3bbb6287f92c4ec751d66e521edd24d7c
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2014-05-23 00:38:46 +0200

    Upload route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Betterlaughs::Application.routes.draw do
 
     root 'laughs#index'
 
+    match '/upload', to: 'laughs#new', via: 'get'
     resources :laughs
     match '/laughs/:id/raw', to: 'laughs#raw', via: 'get'
     match '/laughs/:id/increment_views', to: 'laughs#increment_views', via: 'post'
