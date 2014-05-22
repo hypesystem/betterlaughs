e5dc1cc095e6c4aa3585f7215f056af90347a586
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2014-05-23 00:41:06 +0200

    Routing improvements. Closes #15

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Betterlaughs::Application.routes.draw do
 
     match '/upload', to: 'laughs#new', via: 'get'
     resources :laughs
-    match '/laughs/:id/raw', to: 'laughs#raw', via: 'get'
+    match '/raw/:id', to: 'laughs#raw', via: 'get'
     match '/laughs/:id/increment_views', to: 'laughs#increment_views', via: 'post'
 
     match '/signup', to: 'beta_signups#new', via: 'get'
