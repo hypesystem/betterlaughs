b578eefab4ca13749c3488477b4caabde4edd6ed
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2014-05-23 11:06:57 +0200

    require fields to be filled in #7

--- app/controllers/beta_signups_controller.rb
+++ app/controllers/beta_signups_controller.rb
@@ -4,6 +4,20 @@ class BetaSignupsController < ApplicationController
     def new
     end
 
-    def create
+    def create        
+        signup = params.require(:beta_signup)
+        username = signup.require(:username)
+        email = signup.require(:email)
+        
+        unless valid_email? email
+            raise "invalid email"
+        end
+
+        BetaSignup.new(username: username, email: email)
+        redirect_to laughs_url
+    end
+
+    def valid_email?(email)
+        false
     end
 end
